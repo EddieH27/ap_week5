8e85d13e6bebffb534e4a6b6d463e76d4613bae2
EddieH27 <141260227+EddieH27@users.noreply.github.com>
2025-11-06 18:59:42 +0000

    reverse strings

--- week5_bellRinger.py
+++ week5_bellRinger.py
@@ -36,9 +36,16 @@ john_f_kennedy = print(famous_quote.find("John F. Kennedy"))
 extracted_name = print(famous_quote[83:])
 # Manipulating Words:
 # Given the string info = "Python is fun. Fun is good. Good is subjective.",
+phrase = "Python is fun. Fun is good. Good is subjective."
 # a. Extract the word 'subjective' without knowing its exact position.
+word = print(phrase.find("subjective"))
 # b. Extract every third word.
+every_third = print(phrase[::3])
 # c. Reverse the positions of the words, but keep the characters in each word in the same order.
+words = phrase.split()
+print(words)
+reversed_words = ' '.join(reversed(words))
+print(reversed_words)
 
 # Problem Set 3: String Methods
 # Upper & Lower:
